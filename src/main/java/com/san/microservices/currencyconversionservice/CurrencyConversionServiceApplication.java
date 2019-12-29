4d13705bf1c0d574028f1757981675b400512e3a
sanjeevanand <maxsanjeevanand@gmail.com>
2019-12-29 12:41:24 +0530

    add feign and ribbion configuration

--- src/main/java/com/san/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/san/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.san.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.san.microservices")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
